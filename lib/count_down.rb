a62384937e001f2dce081f06dc51546534f8c65e
Marc Ferraro <marc.ferraro1@gmail.com>
2020-11-14 18:12:27 +0000

    Automatically backed up by Learn

--- lib/count_down.rb
+++ lib/count_down.rb
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 seconds_remaining = 10
 
 while seconds_remaining >= 1
@@ -6,3 +7,12 @@ while seconds_remaining >= 1
   seconds_remaining -= 1
 end
 puts "Happy New Year!"
+=======
+# Write your cmagic_exit_number = 7
+count = 0
+while count < 10 do
+  break if count == magic_exit_number
+  puts "I am the #{count}, I love to count!" # Work
+  count = count + 1
+endode here
+>>>>>>> 20cfc5026df11059c9da0cd542971e1bc2b1178d
